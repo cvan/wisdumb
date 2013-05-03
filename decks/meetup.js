9d6f4181f763eab5e1837390221d2da45aa1569a
Chris Van <cvan@mozilla.com>
2013-05-02 22:29:14 -0700

    hook up all interactions!

--- decks/meetup.js
+++ decks/meetup.js
@@ -12,7 +12,23 @@ deck([
 {
     "id": 2,
     "body": "What are your three most positive, dominant qualities? How will I remember you?",
-    "details": "When attracting/qualifying a girl.",
+    "details": "When attracting/qualifying a girl. Alternatively: \"So who really is _____?\"",
+    "audience": 1,
+    "stranger": true,
+    "stage": "attraction"
+},
+{
+    "id": 3,
+    "body": "I get the feeling that if we were at a nice restaurant, you would pay for everyone there.",
+    "details": "When self-fulfilling-prophecying a girl.",
+    "audience": 1,
+    "stranger": true,
+    "stage": "attraction"
+},
+{
+    "id": 4,
+    "body": "Can you feel this? Does this feel like boyfriend material?",
+    "details": "When a girl likes your suit/outfit.",
     "audience": 1,
     "stranger": true,
     "stage": "attraction"
--- main.js
+++ main.js
@@ -22,7 +22,9 @@ function escape_(s) {
 }
 
 function newCard(card) {
-    return '<article id="card-' + escape_(card.id) + '" data-id="' + escape_(card.id) + '">' + escape_(card.body) + '<details open>' + escape_(card.details) + '</details></article>';
+    return '<article id="card-' + escape_(card.id) +
+           '" data-id="' + escape_(card.id) + '">' + escape_(card.body) +
+           '<details open>' + escape_(card.details) + '</details></article>';
 }
 
 function deck(items) {
@@ -34,15 +36,27 @@ function deck(items) {
 function draw() {
     // TODO: If no more cards left, say that!
     visible = queue.shift();
-    if (visible) {
-        $('main div').html(newCard(visible));
-    } else {
+
+    if (!visible) {
         $('main div').html("<article>You're on your own!</article>");
         $('main menu').hide();
+        return;
     }
-    setTimeout(function () {
-        $('article, menu').addClass('show');
-    }, 500);
+
+    var rememberedSaid = JSON.parse(localStorage.said || '[]');
+    var rememberedForget = JSON.parse(localStorage.forget || '[]');
+    if (rememberedSaid.indexOf(visible.id) > -1 ||
+        rememberedForget.indexOf(visible.id) > -1) {
+        // Draw another if we've already "said it" or "forget it."
+        return draw();
+    }
+
+    if (visible) {
+        $('main div').html(newCard(visible));
+    }
+    //setTimeout(function () {
+    //$('article, menu').addClass('show');
+    //}, 500);
 }
 
 for (var i = 0; i < decks.length; i++) {
@@ -51,30 +65,54 @@ for (var i = 0; i < decks.length; i++) {
     document.body.appendChild(s);
 }
 
+function saidIt() {
+    said.push(visible);
+
+    // Remember in localStorage.
+    var remembered = JSON.parse(localStorage.said || '[]');
+    remembered.push(visible.id);
+    localStorage.said = JSON.stringify(remembered);
+
+    draw();
+}
+
+function forgetIt() {
+    forget.push(visible);
+
+    // Remember in localStorage.
+    var remembered = JSON.parse(localStorage.forget || '[]');
+    remembered.push(visible.id);
+    localStorage.forget = JSON.stringify(remembered);
+
+    draw();
+}
+
+function skipIt() {
+    skip.push(visible);
+
+    // Push onto the end of the queue.
+    queue.push(visible);
+    draw();
+}
+
 function resetAll() {
     localStorage.clear();
     location.reload();
 }
 
-
 $(document.body).on('click', 'button', function() {
     switch ($(this).attr('class')) {
         case 'said-it':
             console.log('said', visible.id);
-            said.push(visible);
-            draw();
-            break;
-        case 'skip-it':
-            console.log('skip', visible.id);
-            skip.push(visible);
-            // Push onto the end of the queue.
-            queue.push(visible);
-            draw();
+            saidIt(visible);
             break;
         case 'forget-it':
             console.log('forget', visible.id);
-            forget.push(visible);
-            draw();
+            forgetIt(visible);
+            break;
+        case 'skip-it':
+            console.log('skip', visible.id);
+            skipIt(visible);
             break;
         case 'view-all':
             console.log('view-all');
@@ -86,5 +124,5 @@ $(document.body).on('click', 'button', function() {
     }
     $('.show').removeClass('show');
 }).on('click', 'h1', function() {
-    resetAll();
+    location.reload();
 });
