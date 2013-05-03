e1106c2cbe08dc0ceda6115465d22f808b5b1848
Chris Van <cvan@mozilla.com>
2013-05-02 23:17:54 -0700

    put everything in a closure

--- main.js
+++ main.js
@@ -1,129 +1,135 @@
-var decks = ['meetup'];
-var cards = [];
-var queue = [];
-var s;
-var visible;
+(function() {
+    var decks = ['meetup'];
+    var cards = [];
+    var queue = [];
+    var s;
+    var visible;
 
-var said = [];
-var skip = [];
-var forget = [];
+    var said = [];
+    var skip = [];
+    var forget = [];
 
 
-function escape_(s) {
-    if (typeof s === 'string') {
-        return s.replace(/&/g, '&amp;')
-                .replace(/>/g, '&gt;')
-                .replace(/</g, '&lt;')
-                .replace(/'/g, '&#39;')
-                .replace(/"/g, '&#34;');
-    } else {
-        return s;
-    }
-}
-
-function newCard(card) {
-    return '<article id="card-' + escape_(card.id) +
-           '" data-id="' + escape_(card.id) + '">' + escape_(card.body) +
-           '<details open>' + escape_(card.details) + '</details></article>';
-}
-
-function deck(items) {
-    // Append this deck's cards to the global `cards` array.
-    cards = cards.concat(items);
-
-    // Append this deck's cards to the global `queue` array.
-    queue = queue.concat(items);
-}
-
-function draw() {
-    visible = queue.shift();
-
-    if (!visible) {
-        $('main div').html("<article>You're on your own!</article>");
-        $('main menu').hide();
-        return;
+    function escape_(s) {
+        if (typeof s === 'string') {
+            return s.replace(/&/g, '&amp;')
+                    .replace(/>/g, '&gt;')
+                    .replace(/</g, '&lt;')
+                    .replace(/'/g, '&#39;')
+                    .replace(/"/g, '&#34;');
+        } else {
+            return s;
+        }
     }
 
-    var rememberedSaid = JSON.parse(localStorage.said || '[]');
-    var rememberedForget = JSON.parse(localStorage.forget || '[]');
-    if (rememberedSaid.indexOf(visible.id) > -1 ||
-        rememberedForget.indexOf(visible.id) > -1) {
-        // Draw another if we've already "said it" or "forget it."
-        return draw();
+    function newCard(card) {
+        return '<article id="card-' + escape_(card.id) +
+               '" data-id="' + escape_(card.id) + '">' + escape_(card.body) +
+               '<details open>' + escape_(card.details) + '</details></article>';
     }
 
-    $('main div').html(newCard(visible));
-}
+    function deck(items) {
+        // Append this deck's cards to the global `cards` array.
+        cards = cards.concat(items);
 
-for (var i = 0; i < decks.length; i++) {
-    s = document.createElement('script');
-    s.src = 'decks/' + decks[i] + '.js';
-    if (i == 0) {
-        // Draw only the first card from the first desk.
-        s.onload = draw;
+        // Append this deck's cards to the global `queue` array.
+        queue = queue.concat(items);
     }
-    // Append `<script>` tag to `<body>` element.
-    document.body.appendChild(s);
-}
 
-function saidIt() {
-    said.push(visible);
+    // So each `deck/*.js` script can initialize its `deck` (à la JSONP).
+    window.deck = deck;
 
-    // Remember in localStorage.
-    var remembered = JSON.parse(localStorage.said || '[]');
-    remembered.push(visible.id);
-    localStorage.said = JSON.stringify(remembered);
+    function draw() {
+        visible = queue.shift();
 
-    draw();
-}
+        if (!visible) {
+            $('main div').html("<article>You're on your own!</article>");
+            $('main menu').hide();
+            return;
+        }
 
-function forgetIt() {
-    forget.push(visible);
+        var rememberedSaid = JSON.parse(localStorage.said || '[]');
+        var rememberedForget = JSON.parse(localStorage.forget || '[]');
+        if (rememberedSaid.indexOf(visible.id) > -1 ||
+            rememberedForget.indexOf(visible.id) > -1) {
+            // Draw another if we've already "said it" or "forget it."
+            return draw();
+        }
 
-    // Remember in localStorage.
-    var remembered = JSON.parse(localStorage.forget || '[]');
-    remembered.push(visible.id);
-    localStorage.forget = JSON.stringify(remembered);
-
-    draw();
-}
-
-function skipIt() {
-    skip.push(visible);
-
-    // Push onto the end of the queue.
-    queue.push(visible);
-    draw();
-}
-
-function resetAll() {
-    localStorage.clear();
-    location.reload();
-}
-
-$(document.body).on('click', 'button', function() {
-    switch ($(this).attr('class')) {
-        case 'said-it':
-            console.log('said', visible.id);
-            saidIt(visible);
-            break;
-        case 'forget-it':
-            console.log('forget', visible.id);
-            forgetIt(visible);
-            break;
-        case 'skip-it':
-            console.log('skip', visible.id);
-            skipIt(visible);
-            break;
-        case 'view-all':
-            console.log('view-all');
-            break;
-        case 'reset-all':
-            console.log('reset-all');
-            resetAll();
-            break;
+        $('main div').html(newCard(visible));
     }
-    $('.show').removeClass('show');
-}).on('click', 'h1', function() {
-    location.reload();
-});
+
+    // Include each `.js` file from all the decks.
+    for (var i = 0; i < decks.length; i++) {
+        s = document.createElement('script');
+        s.src = 'decks/' + decks[i] + '.js';
+        if (i == 0) {
+            // Draw only the first card from the first desk.
+            s.onload = draw;
+        }
+        // Append `<script>` tag to `<body>` element.
+        document.body.appendChild(s);
+    }
+
+    function saidIt() {
+        said.push(visible);
+
+        // Look up the remembered value in `localStorage`. Then store it.
+        var remembered = JSON.parse(localStorage.said || '[]');
+        remembered.push(visible.id);
+        localStorage.said = JSON.stringify(remembered);
+
+        draw();
+    }
+
+    function forgetIt() {
+        forget.push(visible);
+
+        // Look up the remembered value in `localStorage`. Then store it.
+        var remembered = JSON.parse(localStorage.forget || '[]');
+        remembered.push(visible.id);
+        localStorage.forget = JSON.stringify(remembered);
+
+        draw();
+    }
+
+    function skipIt() {
+        skip.push(visible);
+
+        // Push onto the end of the queue.
+        queue.push(visible);
+        draw();
+    }
+
+    function resetAll() {
+        localStorage.clear();
+        location.reload();
+    }
+
+    $(document.body).on('click', 'button', function() {
+        switch ($(this).attr('class')) {
+            case 'said-it':
+                console.log('said', visible.id);
+                saidIt(visible);
+                break;
+            case 'forget-it':
+                console.log('forget', visible.id);
+                forgetIt(visible);
+                break;
+            case 'skip-it':
+                console.log('skip', visible.id);
+                skipIt(visible);
+                break;
+            case 'view-all':
+                console.log('view-all');
+                break;
+            case 'reset-all':
+                console.log('reset-all');
+                resetAll();
+                break;
+        }
+        $('.show').removeClass('show');
+    }).on('click', 'h1', function() {
+        location.reload();
+    });
+})();
