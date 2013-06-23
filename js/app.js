a9ff3b441a0ad7ff320fe41462ba46ac0687775e
Chris Van <cvan@mozilla.com>
2013-06-23 14:13:09 -0700

    powered by angular + parse

---
+++ js/app.js
@@ -0,0 +1,12 @@
+/*global angular */
+/*jshint unused:false */
+'use strict';
+
+/**
+ * The main TodoMVC app module
+ *
+ * @type {angular.Module}
+ */
+var todomvc = angular.module('todomvc', []);
+
+Parse.initialize('FNbbf27LU4JUiKWVLEyNJ1HpCTVyF8aJnSTAqdjK', 'XL9j5nbHUEQaIxkErFN3a27oRrVnzANm7ZzZTszM');
