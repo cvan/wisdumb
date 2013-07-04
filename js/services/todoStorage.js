37a4463489f8422e0436e44e5627bf6e803c50cd
Chris Van <cvan@mozilla.com>
2013-07-04 10:19:39 -0700

    save notes to Parse database backend

--- js/services/todoStorage.js
+++ js/services/todoStorage.js
@@ -5,15 +5,20 @@
  * Services that persists and retrieves TODOs from localStorage
 */
 todomvc.factory('todoStorage', function () {
-    var STORAGE_ID = 'todos-angularjs-perf';
-
+    var STORAGE_ID = 'wisdumb';
+    var Note = Parse.Collection.extend({
+        model: Note
+    });
+    var note = new Note();
     return {
-        get: function () {
-            return JSON.parse(localStorage.getItem(STORAGE_ID) || '[]');
+        get: function() {
+            var value = JSON.parse(localStorage[STORAGE_ID] || '[]');
+            value = note.at(0);
+            return value;
         },
-
-        put: function (todos) {
-            localStorage.setItem(STORAGE_ID, JSON.stringify(todos));
+        put: function(todos) {
+            note.reset(todos);
+            localStorage[STORAGE_ID] = JSON.stringify(todos);
         }
     };
 });
